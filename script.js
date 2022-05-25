cd5c04a76f7446c652e8a7744d46fd4fefa4f6b8
CloudyWhale <83542885+CloudyWhale@users.noreply.github.com>
2022-05-25 19:26:56 -0400

    Make it actually work

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function openWindow() {
+  var element = document.getElementById("container");
+  element.classList.add("open");
+  element.style.transform = "scale(1)";
+  setTimeout(function(){
+    element.classList.remove("open");
+}, 1000);
+}
 
+function closeWindow() {
+  var element = document.getElementById("container");
+  element.classList.add("close");
+  element.style.transform = "scale(0)";
+  setTimeout(function(){
+    element.classList.remove("close");
+}, 1000);
+}
